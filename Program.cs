377382e076b309af5e11878cfff623e87fa562bf
Ksmitty99 <smi17010@byui.edu>
2021-09-30 15:49:56 -0600

    FINALLY

--- Program.cs
+++ Program.cs
@@ -7,6 +7,27 @@ namespace cse201_tc03
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to our Collaborative Program!");
+
+
+
+
+            greetingKody();
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+        static void greetingKody()
+        {
+            Console.WriteLine("Hello from Kody Smith!")
+        }
